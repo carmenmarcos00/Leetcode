130f608ff19d2fb6627cc85a18f392040b55c176
Carmen Marcos <67064895+carmenmarcos00@users.noreply.github.com>
2022-10-07 16:04:19 +0200

    Create 13.Roman_to_Integer(Easy).py

---
+++ 13.Roman_to_Integer(Easy).py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        previousValue = 99999
+        actualValue = 0
+        total = 0
+        
+        dictTranslations = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+            
+        for romanNumber in s:                                                     # Iterate all the values in roman notation
+            actualValue = dictTranslations[romanNumber]                          
+            if (previousValue <actualValue):                                      # If actual value is bigger than the previous one I have to substract
+                total = total - previousValue                                         # Substract the previous one that I already added
+                total = total  + (actualValue - previousValue)                        # Add the diference between the previous and the actual
+        
+            else:
+                total = total + actualValue                                       # If is smaller than the previous one I add the value
+            previousValue = actualValue                                     
+        return total                                                        # When all iterations are finished, return the value
